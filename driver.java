e1bee70667f5932e240d3428dd024ef4f709b82f
alex chalyy <alexchalyy@gmail.com>
2019-10-29 18:56:28 -0400

    word count seperated into new class, everything works

--- driver.java
+++ driver.java
@@ -6,33 +6,6 @@ import java.io.IOException;
 class JavaCodingChallenge {
 
 //---------------------------------------------------------------------------------------
-
-    static void WordCount() {
-
-        // This method opens the text files, counts the words in it, and displays it on
-        // console.
-
-        try {
-            File f1 = new File("passage.txt"); // Creation of File Descriptor for input file
-            String[] words = null; // Intialize the word Array
-            int wc = 0; // Intialize word count to zero
-            FileReader fr = new FileReader(f1); // Creation of File Reader object
-            BufferedReader br = new BufferedReader(fr); // Creation of BufferedReader object
-            String s;
-
-            while ((s = br.readLine()) != null) // Reading Content from the file
-            {
-                words = s.split(" "); // Split the word using space
-                wc = wc + words.length; // increase the word count for each word
-            }
-            fr.close();
-            System.out.println("Number of words in the file:" + (wc - 2)); // Print the word count
-        } catch (Exception e) {
-            System.out.println("Something did not work:(");
-        }
-    }
-
-    //---------------------------------------------------------------------------------------------
     public static void main(String args[]) throws IOException
 
     /*
@@ -51,6 +24,8 @@ class JavaCodingChallenge {
      */
 
     {
-        WordCount(); // Give a total word count.
+        WordCount wc = new WordCount(); 
+
+        wc.CountWords(); // Give a total word count.
     }
 }
